2ab736e1c9e44004d0a56a5acb401ffc6e0f49e0
liuzijing <lzj4103@126.com>
2022-04-11 10:21:37 +0800

    上传一下webpage

--- vite.config.ts
+++ vite.config.ts
@@ -9,22 +9,22 @@ export default defineConfig({
       '@':'/src'
     }
   },
-  build: {
-    rollupOptions: {
-      // 请确保外部化那些你的库中不需要的依赖
-      external: ['vue','vue-router'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-    lib: {
-      entry: './src/packages/index.ts',
-      name: 'smartisan-ui',
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     // 请确保外部化那些你的库中不需要的依赖
+  //     external: ['vue','vue-router'],
+  //     output: {
+  //       // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+  //       globals: {
+  //         vue: 'Vue',
+  //       },
+  //     },
+  //   },
+  //   lib: {
+  //     entry: './src/packages/index.ts',
+  //     name: 'smartisan-ui',
+  //   },
+  // },
   css: {
     // css预处理器
     preprocessorOptions: {
