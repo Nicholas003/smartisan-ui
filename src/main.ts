ce30cc7670deb82b182df6fc6b5b9870151506a0
zijing.liu <e_liuzijing@enn.cn>
2025-03-26 10:26:57 +0800

    feat:处理cicd打包

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index'
-// import Smartisan from '@/packages/index'
-import Smartisan from '../dist/smartisan-ui.es.js'
-import '../dist/style.css'
+import Smartisan from '@/packages/index'
+// import Smartisan from '../dist/smartisan-ui.es.js'
+// import '../dist/style.css'
 document.body.addEventListener('touchstart', function () { });
 
 let app = createApp(App)
