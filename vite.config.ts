cfe271bc7486cb72052111a0ef99de0bae3166b8
zijing.liu <e_liuzijing@enn.cn>
2025-03-26 10:31:19 +0800

    feat:处理gitpage

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'smartisan-ui',
   plugins: [vue(),vueJsx()],
   resolve:{
     alias:{
